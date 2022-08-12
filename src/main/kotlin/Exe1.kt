61f5724b8b04966a6eb0106daaa4fdaf98034f2a
Hickdpaula <hickdpaula@gmail.com>
2022-08-12 14:15:44 -0300

    first commit

---
+++ src/main/kotlin/Exe1.kt
@@ -0,0 +1,18 @@
+fun main () {
+    var resultado: Int = 0
+
+    println("Digite um numero: ")
+    var num1: Int = readLine()!!.toInt()
+
+    println("Digite outro numero: ")
+    val num2: Int = readLine()!!.toInt()
+
+    resultado = num1 + num2
+
+    if (resultado % 2 == 0) {
+        println("a soma dos dois numero é: " + resultado + " e o numero é Par")
+    } else {
+        println("a soma dos dois numero é: " + resultado + " e o numero é Ímpar")
+
+    }
+}
